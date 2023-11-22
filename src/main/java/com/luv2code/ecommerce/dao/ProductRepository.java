b05032161130359c19ef649bb1a5ebc4a6be83f7
jburns6789 <117755326+jburns6789@users.noreply.github.com>
2023-11-22 08:32:47 -0500

    halfway through spring ecommerce

---
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.luv2code.ecommerce.dao;
+
+import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable); // spring will execute a similar query of select * from where category_id =?
+
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable page); // same as above w/ SQL Queries
+}
