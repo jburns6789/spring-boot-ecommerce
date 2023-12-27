c96285cc26c318223414a4508cc767f4ce65189e
jburns6789 <117755326+jburns6789@users.noreply.github.com>
2023-12-27 11:33:58 -0500

    change db name to match mysql in connection string

--- src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
+++ src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
@@ -2,12 +2,21 @@ package com.luv2code.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootEcommerceApplication {
+public class SpringBootEcommerceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootEcommerceApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootEcommerceApplication.class);
+	}
+
+
 
 }
